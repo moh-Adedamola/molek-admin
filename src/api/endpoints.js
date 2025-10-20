5a73837f27d97dd4fa85b4b77df95972096f4bbc
Tbelzbby <belloafeez28@gmail.com>
2025-10-20 22:58:13 +0100

    new push

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+const API_BASE = import.meta.env.VITE_API_BASE_URL 
+console.log("API_BASE URL:", API_BASE);
 
 const api = axios.create({
   baseURL: API_BASE,
